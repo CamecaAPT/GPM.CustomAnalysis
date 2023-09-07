8f149da9ebc927905dd4bca2d57cf7e3b5b56ed1
klaesben <118187611+klaesben@users.noreply.github.com>
2023-09-07 14:39:03 +0200

    Add files via upload

---
+++ Custom_analyses_07092023/GPM.CustomAnalyses/Analyses/LoadMemoryM/LoadMemoryMMenuFactory.cs
@@ -0,0 +1,16 @@
+﻿using Cameca.CustomAnalysis.Interface;
+using Cameca.CustomAnalysis.Utilities;
+using Prism.Events;
+
+namespace GPM.CustomAnalyses.Analyses.LoadMemoryM;
+
+internal class LoadMemoryMMenuFactory : AnalysisMenuFactoryBase
+{
+	public LoadMemoryMMenuFactory(IEventAggregator eventAggregator) : base(eventAggregator)
+	{
+	}
+
+	protected override INodeDisplayInfo DisplayInfo => LoadMemoryMNode.DisplayInfo;
+	protected override string NodeUniqueId => LoadMemoryMNode.UniqueId;
+	public override AnalysisMenuLocation Location { get; } = AnalysisMenuLocation.Analysis;
+}
