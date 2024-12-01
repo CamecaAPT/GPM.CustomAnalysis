1f7f5f3eaa30dd35090edbd2955c0f40c070b26c
Jim Payne <jim.payne@ametek.com>
2024-12-01 15:30:22 -0600

    Remove LiveCharts dependency
    No LiveCharts controls remain, so remove the dependency and remove
    any remaining references.
    
    This avoids bundling extra DLLs that are not needed.
    
    LiveCharts was an example of how third party dependencies could be
    included in a custom analysis and how they could be used to include
    UI elements not exposed by AP Suite or in the standard Microsoft
    control library.

--- GPM.CustomAnalyses/Analyses/ClusterPositionM/ClusterPositionMView.xaml.cs
+++ GPM.CustomAnalyses/Analyses/ClusterPositionM/ClusterPositionMView.xaml.cs
@@ -4,8 +4,6 @@ using System.Windows.Media;
 using System.Linq;
 using System.Windows.Controls;
 using Cameca.CustomAnalysis.Interface;
-using LiveCharts;
-using LiveCharts.Wpf;
 
 namespace GPM.CustomAnalyses.Analyses.ClusterPositionM;
 /// <summary>
@@ -19,20 +17,6 @@ public partial class ClusterPositionMView : UserControl
 		InitializeComponent();
 		ComboBoxKMeans.Items.Add("Manual");
 		ComboBoxKMeans.Items.Add("Auto");
-	}
-
-	public Func<ChartPoint, string> PointLabel { get; set; }
-
-	private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
-	{
-		var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
-
-		//clear selected slice.
-		foreach (PieSeries series in chart.Series)
-		series.PushOut = 0;
-
-		var selectedSeries = (PieSeries)chartpoint.SeriesView;
-		selectedSeries.PushOut = 8;
 	}
 
 	private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
